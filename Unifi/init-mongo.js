78f270a9d6f722331fbe469229d7711d9086c18e
spodsoft <99612131+spodsoft@users.noreply.github.com>
2024-05-10 13:31:49 +0100

    Delete Unifi directory

--- Unifi/init-mongo.js
+++
@@ -1,2 +0,0 @@
-db.getSiblingDB("unifi").createUser({user: "USERNAME", pwd: "PASSWORD", roles: [{role: "dbOwner", db: "unifi"}]});
-db.getSiblingDB("unifi_stat").createUser({user: "USERNAME", pwd: "PASSWORD", roles: [{role: "dbOwner", db: "unifi_stat"}]});
